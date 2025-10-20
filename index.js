2480050ae864f5476a39cab0c386f957f0f7e769
asyiraafnufail <asyiraaf4@gmail.com>
2025-10-20 09:45:11 +0700

    modify index.js

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+let mysql = require('mysql2/promise');
+const app = express();
+const port = 3000;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'mahasiswa',
+    port : 3307
+});
+
+db.connect(err => {
+    if (err) {
+        console.error('Error connecting to the database:', err);
+        return;
+    }
+    console.log('Connected to the MySQL database.');    
+})
+
+db.connect()
+  .then(() => console.log('Database connected'))
+  .catch(err => console.error('Database connection failed:', err));
